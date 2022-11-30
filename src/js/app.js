71a9f6b4a5f0418c4b7177ef298520a6e2900d71
Samir Azzam <40363140+WestsideBG@users.noreply.github.com>
2022-11-30 15:32:56 +0200

    added emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let hotPrices = document.getElementsByClassName("hot")
+
+  for (let index = 0; index < hotPrices.length; index++) {
+    const element = hotPrices[index];
+    hotPrices[index].insertAdjacentText('beforeEnd'," 🔥")
+  }
+
 });
